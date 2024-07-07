4f8eb658a45612f4b43ea337be57ad5dbdb25edd
Alex <aleksandrdorofeev04@gmail.com>
2024-07-08 00:39:32 +0500

    improve kratos

--- characters/character.gd
+++ characters/character.gd
@@ -5,16 +5,20 @@ extends CharacterBody2D
 @export var acceleration: int = 1500 # Ускорение
 @export var friction: int = 1000 # Трение
 
+@export var max_hp: int = 100
+@export var hp: int = max_hp
+@export var armor: int = 0 # Процентное снижение урона (0-100)
+
 var direction: Vector2 = Vector2.ZERO # Направление движения
 
 func move(delta: float) -> void:
-	if direction: 
+	if direction:
 		velocity = velocity.move_toward(direction * max_speed, delta * acceleration)
-	else: 
+	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * friction)
 		
 	move_and_slide()
 
 
 func take_damage(damage: int) -> void:
-	pass
+	hp -= damage - (damage * int(armor / 100.0))
--- characters/players/kratos/kratos.gd
+++ characters/players/kratos/kratos.gd
@@ -1,24 +1,43 @@
-@icon("res://assets/1/hero1.png")
+@icon("res://assets/1/kratos_picture.png")
 extends Player
 class_name Kratos
+
+func take_damage(damage: int):
+	super(damage)
+	$HealthBar.value = hp
+	if hp <= 0:
+		$StateChart.send_event("dead")
+	else:
+		$StateChart.send_event("hurt")
 
 func _physics_process(delta):
 	super(delta)
 	if direction != Vector2.ZERO:
-		$AnimationTree.set("parameters/StateMachine/idle/blend_position", direction)
-		$AnimationTree.set("parameters/StateMachine/move/blend_position", direction)
+		$AnimationTree.set("parameters/movement/idle/blend_position", direction)
+		$AnimationTree.set("parameters/movement/move/blend_position", direction)
+
+	if Input.is_action_just_pressed("debug"):
+		take_damage(20)
 
 
 func _on_afk_state_entered():
-	$AnimationTree["parameters/StateMachine/playback"].travel("afk")
+	$AnimationTree["parameters/movement/playback"].travel("afk")
 
 
 func _on_idle_state_entered():
-	$AnimationTree["parameters/StateMachine/playback"].travel("idle")
+	$AnimationTree["parameters/movement/playback"].travel("idle")
 
 
 func _on_move_state_entered():
-	$AnimationTree["parameters/StateMachine/playback"].travel("move")
+	$AnimationTree["parameters/movement/playback"].travel("move")
+
+
+func _on_hurt_state_entered():
+	$HitFlashAnimationPlayer.play("hurt")
+
+
+func _on_dead_state_entered():
+	queue_free()
 
 
 func _on_afk_state_physics_processing(_delta):
@@ -35,3 +54,14 @@ func _on_move_state_physics_processing(_delta):
 	if direction == Vector2.ZERO:
 		$StateChart.send_event("idle")
 		$StateChart.send_event("afk")
+
+
+func _on_hurt_state_physics_processing(_delta):
+	if direction == Vector2.ZERO:
+		$StateChart.send_event("idle")
+		$StateChart.send_event("afk")
+	else:
+		$StateChart.send_event("moving")
+
+
+
