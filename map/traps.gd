7b4eb1e77ac8339d5ade8949d8b21696f3df43b2
not animal <sozykin25@gmail.com>
2024-07-07 20:30:52 +0500

    added traps particles

--- map/traps.gd
+++ map/traps.gd
@@ -2,5 +2,20 @@ extends Area2D
 func _ready()->void:
 	$AnimatedSprite2D.play("active")
 func _on_body_entered(_body: Node2D) -> void:
-	$AnimatedSprite2D.play("nonactive")
+	while($AnimatedSprite2D.animation==("active")):
+		if(_body.name=="Kratos"):
+			$GPUParticles2D.emitting = true
+			$AnimatedSprite2D.play("nonactive")
+		else:
+			$GPUParticles2D.emitting = false
+
+		
 	
+		
+		
+	
+	
+	
+
+
+
